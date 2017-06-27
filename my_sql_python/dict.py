ffdb988a4854558ad305acf2c1e42974a1c2ef9b
Kaptiug111 <Mickey_banks@mail.ru>
2017-06-27 19:54:24 +0300

    first task

---
+++ my_sql_python/dict.py
@@ -0,0 +1,76 @@
+import mysql.connector
+from mysql.connector import errorcode
+import time
+
+query_dict = {
+    "select_all" : "SELECT  * FROM new_base.new_table",
+    "select_by_id" : "SELECT * FROM new_base.new_table WHERE new_base.new_table.idnew_table={0}"
+    "insert": "INSERT INTO new_base.new_table VALUES({0})"
+}
+
+def commonSelect (query):
+    print 0
+    returnvalue = None
+    try:
+        cnx = mysql.connector.connect(user='root', password='ma',
+                                      host='127.0.0.1',
+                                      database='new_base')
+        cursor = cnx.cursor()
+
+        cursor.execute(query)
+
+        returnvalue = list()
+        for line in cursor:
+            returnvalue.append(line)
+
+        cursor.close()
+
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
+    return returnvalue
+
+def selectAll():
+    return commonSelect(query_dict["select_all"])
+
+def selectByID(id):
+    return commonSelect(query_dict["select_by_id"].format(id))
+
+
+print(selectByID(1))
+
+
+
+#insert value
+
+def commonInsert (query):
+    returnvalue = None
+	try:
+	    cnx = mysql.connector.connect(user='root', password='ma',
+		                          host='127.0.0.1',
+		                          database='new_base')
+	    cursor = cnx.cursor()
+
+	    cursor.execute(query)
+
+	    cnx.commit()
+	    cursor.close()
+
+	except mysql.connector.Error as err:
+	    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+		print("Something is wrong with your user name or password")
+	    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+		print("Database does not exist")
+	    else:
+		print(err)
+	else:
+	    cnx.close()
+
+
+
+	
+
